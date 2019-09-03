4d8ebd9c42719f3879a51b61cc44a22e5bacb63c
Łukasz Spalczyński <spali89@gmail.com>
2019-09-03 17:08:08 +0200

    fix eslint issue

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+const Icon = props => (<i className={`fas fa-${props.name}`}/>);
+
+Icon.propTypes = {
+  name: PropTypes.node,
+};
 
 export default Icon;
+
+
