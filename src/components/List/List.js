7219f67028bcc19ac7791826c89b66506adb838a
Łukasz Spalczyński <spali89@gmail.com>
2019-09-10 18:54:10 +0200

    add containter

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator';
+import Container from '../Container/Container.js';
 
 class List extends React.Component {
   static propTypes = {
@@ -26,21 +27,20 @@ class List extends React.Component {
 
     return (
       <section className={styles.component}>
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        <div className={styles.columns}>
-          {columns.map(columnData =>
-            <Column key={columnData.id} {...columnData} />
-          )}
-        </div>
-        <div className={styles.creator}>
-          <Creator
-            text={settings.columnCreatorText}
-            action={addColumn}
-          />
-        </div>
+        <Container>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData =>
+              <Column key={columnData.id} {...columnData} />
+            )}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </Container>
       </section>
     );
   }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from './Search.scss';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-import { settings } from '../../data/dataStore';
+import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon';
+import Container from '../Container/Container.js';
 
 class Search extends React.Component {
   static propTypes = {
@@ -13,28 +14,28 @@ class Search extends React.Component {
     changeSearchString: PropTypes.func,
     countVisible: PropTypes.number,
     countAll: PropTypes.number,
-  }
+  };
 
   static defaultProps = {
     text: settings.search.defaultText,
     icon: settings.search.icon,
-  }
+  };
 
   state = {
     value: this.props.searchString || '',
-  }
+  };
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       value: event.target.value,
     });
   }
 
-  handleOK(){
+  handleOK() {
     this.props.changeSearchString(this.state.value);
   }
 
-  UNSAFE_componentWillReceiveProps(newProps){
+  UNSAFE_componentWillReceiveProps(newProps) {
     this.setState({value: newProps.searchString});
   }
 
@@ -43,18 +44,22 @@ class Search extends React.Component {
     const {value} = this.state;
     return (
       <div className={styles.component}>
-        <input
-          type='text'
-          placeholder={text}
-          value={value}
-          onChange={event => this.handleChange(event)}
-        />
-        <div className={styles.buttons}>
-          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
-        </div>
-        <div>
-          { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
-        </div>
+        <Container>
+          <input
+            type="text"
+            placeholder={text}
+            value={value}
+            onChange={event => this.handleChange(event)}
+          />
+          <div className={styles.buttons}>
+            <Button onClick={() => this.handleOK()}>
+              <Icon name={icon} />
+            </Button>
+          </div>
+          <div>
+            {countVisible == countAll ? '' : `${countVisible} / ${countAll}`}
+          </div>
+        </Container>
       </div>
     );
   }
